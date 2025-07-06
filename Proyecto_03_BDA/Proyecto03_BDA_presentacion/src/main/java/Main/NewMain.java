21911568fdc7f7efb89eef4ebd66913527e80169
adrian252699 <jesus.luzanilla252699@potros.itson.edu.mx>
2025-07-05 23:49:56 -0700

    Pantallas

---
+++ Proyecto_03_BDA/Proyecto03_BDA_presentacion/src/main/java/Main/NewMain.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Main;
+
+import inicio.Inicio;
+
+/**
+ *
+ * @author jalt2
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Inicio frmInicio = new Inicio();
+        
+        frmInicio.setVisible(true);
+    }
+    
+}
